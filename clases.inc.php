e4ce95bb10ed5a044c94a5956c7babd1442f89e8
Jorge Diaz <jldiaz@defco.com.ar>
2018-01-11 16:43:23 -0300

    Arregla ruta del autoload

--- clases.inc.php
+++ clases.inc.php
@@ -3,7 +3,7 @@
 	define("PROJECTPATH", dirname(__DIR__));
 
 	spl_autoload_register(function($class) {
-	    $filename = PROJECTPATH . '/backend/' . $class . '.class.php';
+	    $filename = PROJECTPATH . '/webservice/' . $class . '.class.php';
 	    $filename_real = str_replace(['/',"\\"],DIRECTORY_SEPARATOR,$filename);
 	    require $filename_real; 
 	});
